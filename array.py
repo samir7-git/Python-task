94dbe418d115c65c7351c2325943f77c2677ec90
Samir Mujawar <samirmujawar000@gmail.com>
2024-06-30 23:31:55 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,15 @@
+
+
+#task :- create a function which prints all the values in array and callback that function.
+
+# Creating an array (list) in Python
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Defining a function to print all values in the array
+def print_my_array(array):
+    for value in array:
+        print(value)
+
+# Calling the function and passing the array as an argument
+
+print_my_array(my_array)
